b61698a03b24c87abb5cf99894950cee90710af2
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-12 07:24:25 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,10 +26,10 @@ class Scraper
      
      def make_courses
        self.get_courses.each do |post|
-         course = Course.new
-      course.title = post.css("h2").text          #giving instance title, schedule, descirption
-      course.schedule = post.css(".date").text
-      course.description = post.css("p").text
+        course = Course.new
+        course.title = post.css("h2").text          
+        course.schedule = post.css(".date").text
+        course.description = post.css("p").text
          
     # doc.css(".post").each do |post|
     #   course = Course.new
