c782ccae1c386cbb0ea0a2518670d920f9a3bf91
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-12 07:22:01 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,7 +17,10 @@ class Scraper
   end
     def get_page
       doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+     end
      
+     def get_courses
+       self.get_page.css("post")
     # doc.css(".post").each do |post|
     #   course = Course.new
     #   course.title = post.css("h2").text          #giving instance title, schedule, descirption
